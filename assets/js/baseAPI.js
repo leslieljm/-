8d503b9813d8c958945edb558e9bd9444b0bd25b
leslie <1951672525@qq.com>
2022-08-04 11:20:07 +0800

    index页面完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,7 +4,28 @@ const baseUrl = 'http://www.liulongbin.top:3007'
 // 需要先导入这个baseAPI.js文件
 
 $.ajaxPrefilter(option => {
+
+    // 以/my开头的请求路径，统一需要在请求头中携带 Authorization 身份认证字段
+    if (option.url.includes('/my/')) {
+        option.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
     option.url = baseUrl + option.url  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+
+    // 响应拦截器
+    // 控制用户的访问权限：用户如果没有登录，是否能够允许用户访问后台主页？肯定是不能的，所以我们需要进行权限的校验，可以利用请求后服务器返回的状态来决定
+    // 在调用有权限接口的时候，指定 `complete` 回调函数，这个回调函数不管成功还是失败都会调用
+    // 在回调里面判断 服务器返回的的状态是否等于 1，并且错误的信息是  "身份认证失败"，如果成立，那么就强制用户跳转到登录页
+    option.complete = res => {
+        console.log(res);
+        // 如果没有登录，
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // console.log('没有token');
+            location.href = '/login.html'
+        }
+    }
 })
 
 // ajaxPrefilter()函数接收一个回调函数为参数，回调函数里形参option接收的实参是$.get() 或 $.post() 或 $.ajax()里的参数对象
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+// 获取用户基本信息
+const getUserInfo = () => {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        data: null,
+        /* headers: {
+            Authorization: localStorage.getItem('token')
+        }, */
+        success: res => {
+            // console.log(res);
+            const {status, data} = res
+            if (status !== 0) return layui.layer.msg("数据请求失败！")
+            renderAvatar(data)
+        }
+    })
+}
+
+const renderAvatar = data => {
+    let name = data.nickname || data.username
+    // console.log(name);
+    // 设置欢迎文本
+    $('#welcome').html('欢迎' + name)
+    // 如果有头像
+    if (data.user_pic !== null) {
+        $('.layui-nav-img').attr('src', data.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        // 如果没有头像
+        $('.layui-nav-img').hide()
+        let firstName = name[0].toUpperCase()
+        $('.text-avatar').html(firstName)
+    }
+}
+
+getUserInfo()
+
+// 点击退出登录按钮
+$('#exitBtn').click(function () {
+    // 利用layui组件的询问弹出层
+    layer.confirm('确定退出？', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        // 跳转到登陆页面，注意一定要清空本地存储里面的 token
+        location.href = '/login.html'
+        localStorage.removeItem('token')
+        layer.close(index);
+    });
+})
